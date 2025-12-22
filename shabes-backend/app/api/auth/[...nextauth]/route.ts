f9ae7c5a6b925ed9e4a5f4d98152c740220ad112
Dyego Melo <dyegodomingos@gmail.com>
2025-12-22 01:36:09 -0300

    fix: corrigindo import path para relativo

--- shabes-backend/app/api/auth/[...nextauth]/route.ts
+++ shabes-backend/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/app/lib/auth'; // Importando do arquivo que criamos acima
+import { authOptions } from "../../../lib/auth";
 
 const handler = NextAuth(authOptions);
 
